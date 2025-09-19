0c6553c0f55c6d818fe9cfbd3d167fb0720470f3
RazorDKK <141557634+RazorDKK@users.noreply.github.com>
2025-09-19 10:37:54 +0200

    tilføjer pointskema
    Den tjekker for 1'er op til 6'er og for 3 ens

---
+++ terninger.js
@@ -0,0 +1,14 @@
+class terning {
+  constructor(number, x, y) {
+    this.number = number;
+    this.x = x;
+    this.y = y;
+  }
+  draw() {}
+
+  roll() {
+    if ((this.number = 0)) {
+      this.number = floor(random(1, 6));
+    }
+  }
+}
