9b554018709fa2fba8af5dfe3233d355084a6cfa
RazorDKK <141557634+RazorDKK@users.noreply.github.com>
2025-09-19 08:45:34 +0200

    tilføjer klassen terninger

---
+++ terninger.js
@@ -0,0 +1,13 @@
+class terning {
+  constructor(number, x, y) {
+    this.number = number;
+    this.x = x;
+    this.y = y;
+  }
+
+  roll() {
+    if ((this.number = 0)) {
+      this.number = floor(random(1, 6));
+    }
+  }
+}
